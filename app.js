bdf7bbf90eb2fdf0caa94f33ba93e56930e1a95c
Kellen James <Kellenkjames@gmail.com>
2020-07-01 22:08:05 -0700

    lax breakpoints
    defined breakpoints for lax.js animations

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 window.onload = function() {
-	lax.setup() // init
+	
+	lax.setup({
+		breakpoints: { mobile: 576, tablet: 768, laptop: 992, desktop: 1200 }
+	})
 
 	const updateLax = () => {
 		lax.update(window.scrollY)
