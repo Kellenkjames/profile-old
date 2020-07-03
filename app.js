49a0e05e3f1e56ec087de52efbc50e36ea5ec264
Kellen James <Kellenkjames@gmail.com>
2020-07-02 19:09:40 -0700

    breakpoints adjusted for 992 and up lax.js
    breakpoints adjusted for 992 and up lax.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 	
 	lax.setup({
-		breakpoints: { mobile: 576, tablet: 768, laptop: 992, desktop: 1200 }
+		breakpoints: { small: 576, medium: 768, large: 992 }
 	})
 
 	const updateLax = () => {
